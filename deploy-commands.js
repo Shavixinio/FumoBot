bd7e95557627511858ef452d542153c24a69d91d
Shavix <mrmater121@gmail.com>
2024-12-18 20:54:58 +0100

    I dont even know what I did there

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,33 +1,71 @@
-const { REST, Routes } = require('discord.js');
-const fs = require('fs');
-require('dotenv').config(); // Load environment variables from .env file
+require("dotenv").config();
+const { REST, Routes } = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
+const token = process.env.TOKEN;
+const clientId = process.env.CLIENT_ID;
+const guildId = process.env.GUILD_ID;
 
-// Get all commands from the commands folder
-const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+function removeCommands() {
+  rest
+    .put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
+    .then(() => console.log("Successfully deleted all guild commands."))
+    .catch(console.error);
 
-// Loop over all the files in the commands folder and add them to the commands array
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    commands.push(command.data.toJSON());
+  // for global commands
+  rest
+    .put(Routes.applicationCommands(clientId), { body: [] })
+    .then(() => console.log("Successfully deleted all application commands."))
+    .catch(console.error);
 }
 
-// Create a new REST instance
-const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+const commands = [];
+// Grab all the command folders from the commands directory you created earlier
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
 
-// Refresh the commands
-(async () => {
-    try {
-        console.log('Started refreshing application (/) commands.');
-        // Put the commands in the guild
-        await rest.put(
-            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
-            { body: commands },
-        );
-
-        console.log('Successfully reloaded application (/) commands.');
-    } catch (error) {
-        // Catch any errors and log them to the console
-        console.error(error);
+for (const folder of commandFolders) {
+  // Grab all the command files from the commands directory you created earlier
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ("data" in command && "execute" in command) {
+      commands.push(command.data.toJSON());
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
     }
+  }
+}
+
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(token);
+
+// and deploy your commands!
+(async () => {
+  try {
+    console.log(
+      `Started refreshing ${commands.length} application (/) commands.`
+    );
+    console.log(commands);
+    // removeCommands();
+
+    // The put method is used to fully refresh all commands in the guild with the current set
+    const data = await rest.put(Routes.applicationCommands(clientId), {
+      body: commands,
+    });
+
+    console.log(
+      `Successfully reloaded ${data.length} application (/) commands.`
+    );
+  } catch (error) {
+    // And of course, make sure you catch and log any errors!
+    console.error(error);
+  }
 })();
