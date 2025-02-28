c79255d1bc39872a9265976f7885040858451585
Shavixinio <mrmater121@gmail.com>
2025-02-28 14:28:02 +0100

    Make an embed for the commands (not tested)

--- commands/general/help.js
+++ commands/general/help.js
@@ -1,11 +1,19 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('help')
         .setDescription('Shows all available commands'),
-        // TODO: Make an embed that list all commands
         async execute (interaction) {
-            await interaction.reply({content: "test", ephermal: true});
+            const embed = new EmbedBuilder()
+            .setTitle("Commands")
+            .setDescription(`
+                **Moderation**:
+                ban - bans a person
+                kick - kicks a person
+                unban - unbans a person
+                `)
+                .setColor("237feb");
+            await interaction.reply({content: embed, ephermal: true});
         }
 }
