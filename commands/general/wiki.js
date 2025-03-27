385786e469d8a16303e075c4d71f9465daa041cb
Shavixinio <mrmater121@gmail.com>
2025-03-27 17:53:43 +0100

    add a wiki search command

---
+++ commands/general/wiki.js
@@ -0,0 +1,33 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const wiki = require('wikipedia');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('wiki')
+        .setDescription('Search for information')
+        .addStringOption(option =>
+            option.setName('query')
+                .setDescription('What you want to search for')
+                .setRequired(true)),
+
+    async execute(interaction) {
+        await interaction.deferReply();
+        const searchQuery = interaction.options.getString('query');
+        
+        try {
+            const searchResults = await wiki.search(searchQuery);
+            const page = await wiki.page(searchResults.results[0].title);
+            const summary = await page.summary();
+            
+            const embed = new EmbedBuilder()
+                .setColor('#0099ff')
+                .setTitle(summary.title)
+                .setDescription(summary.extract)
+                .setURL(summary.content_urls.desktop.page);
+
+            await interaction.editReply({ embeds: [embed] });
+        } catch (error) {
+            await interaction.editReply('Could\'t find any information on that topic or an error occurred');
+        }
+    }
+};
